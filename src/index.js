0d26f59daa3559e839b41811b4543bb0f34ec03d
Ujjawal2507 <ujjawalasthana7943@gmail.com>
2025-07-17 00:50:26 +0530

    added extra files

--- src/index.js
+++ src/index.js
@@ -6,7 +6,16 @@ dotenv.config({
     path:"./env"
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((error) => {
+    console.error("Failed to connect to the database:", error);
+});
+
 
 
 
